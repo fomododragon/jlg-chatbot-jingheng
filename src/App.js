7d20e645750aab88c39579c8404f4a377f2f6afb
tjpink <tjpink@gmail.com>
2021-11-01 21:24:09 +0800

    arrange the sentense to make it sound smoother

--- src/App.js
+++ src/App.js
@@ -40,8 +40,8 @@ function App() {
     IntroSelf: "Hello, I am here to guide you around Jurong Lake Gardens",
     JLG_Map: "You may refer to the jurong lake garden map to get to your destination.",
     Fishing: "Fishing is only allowed in designated fishing areas in some of our parks, please look out for signboards if you are permitted.",
-    Camping: "Camping in our parks is a recreational option for Singapore residents and is allowed in designated areas. Camping areas in our parks are in East Coast Park, Pasir ris park and West Coast Park camping areas, permits are required.",
-    WearMask: "You are required to wear a mask except when engaging in strenuous exercise, such as running, cycling, brisk walking, and walking in hilly terrain. Please refer to our website at www.nparks.gov.sg/noticeboard for the latest advisories during the covid-19 situation.",
+    Camping: "Camping in our parks is a recreational option for Singapore residents, and is allowed in designated areas. Camping areas in our parks are in East Coast Park, Pasir ris park and West Coast Park camping areas, permits are required.",
+    WearMask: "You are required to wear a mask except when engaging in strenuous exercise, such as running, cycling, brisk walking, and walking in hilly terrain. For more information, please visit our website at www.nparks.gov.sg/noticeboard.",
     Cycling: "You are welcome to cycle along our park connectors, or in parks with designated cycling tracks and trails. Please give way to pedestrians on footpath and shared paths.",
     BookVenue: "In line with latest announcement by the Multi-Ministry TaskForce (MTF) for Phase Two, function spaces will be reopened for wedding solemnisations, subjected to a cap of 20 persons. We are unable to accomodate books for other events and activities until further notice. For more information on latest advisories in response to covid-19 situation, please visit our website at www.nparks.gov.sg/noticeboard."
   };
